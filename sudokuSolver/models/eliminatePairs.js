e70ae68e73c5f7877cb899c78da6c63353e5beb4
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-28 21:46:41 +0100

    Created a function file and test file, ready to start TDD

---
+++ sudokuSolver/models/eliminatePairs.js
@@ -0,0 +1,16 @@
+import potentialAnswers from './potentialAnswers.js'
+
+let example = [
+  [0, 0, 0, 5, 2, 0, 8, 0, 1],
+  [0, 0, 2, 0, 8, 0, 9, 0, 5],
+  [0, 5, 0, 0, 4, 9, 0, 2, 0],
+  [6, 2, 9, 4, 5, 1, 3, 7, 8],
+  [4, 3, 1, 9, 7, 8, 2, 5, 6],
+  [5, 8, 7, 6, 3, 2, 0, 0, 9],
+  [0, 1, 0, 3, 6, 5, 0, 9, 2],
+  [0, 0, 0, 2, 1, 0, 5, 8, 0],
+  [2, 0, 5, 8, 9, 0, 0, 0, 0],
+];
+
+let guesses = potentialAnswers(example)
+console.log(guesses)
