65ceaae476bf87f447f5521157b8f61c98ed03e7
St0neofFr33dom <christophefreeston@googlemail.com>
2022-07-30 16:49:05 +0100

    Edited the html page so that all the boxes have ids, they also have input boxes that only take 1 character and it can only be a number

--- sudokuSolver/main.js
+++ sudokuSolver/main.js
@@ -1,8 +1,17 @@
 let numberBox = document.querySelectorAll(".square")
 console.log(numberBox)
 
-function inputNumber() {
-    let square = document.querySelector(".square")
-    let inputBox = document.createElement("input")
-    square.appendChild(inputBox)
+// function inputNumber() {
+//     let input = prompt("Please input a number between 1 to 9");
+//     Node.innerHTML=input
+// }
+
+// let test = document.querySelector(".square")
+// test.addEventListener("click",inputNumber)
+
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
 }
